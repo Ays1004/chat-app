855f15134484327933ffe4a8e72a248e7447200e
Ays1004 <yewaleayush@gmail.com>
2024-12-30 00:51:20 +0530

    updated messageControllers

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -18,6 +18,18 @@ export const sendMessage = async (req, res) =>{
             })
         }
 
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            message,
+        })
+
+        if(newMessage){
+            conversation.messages.push(newMessage._id);
+        }
+
+        res.status(201).json(newMessage)
+
     } catch (error) {
         console.log("Error in sendMessage controller: ", error.message)
         res.status(500).json({ error: "Internal Server Error" });
