5d7c64b3879173b446999201a3bb140941e10851
Ays1004 <yewaleayush@gmail.com>
2024-12-30 00:47:00 +0530

    added message controller

---
+++ server/controllers/messageController.js
@@ -0,0 +1,23 @@
+import Conversation from "../models/conversationModel.js";
+
+export const sendMessage = async (req, res) =>{
+    try {
+        const {message} = req.body;
+        const {id: receiverId} = req.params;
+        const senderId = req.userId;
+
+        const conversation =await Conversation.findOne({
+            participants:{
+                $all: {senderId, receiverId}
+            }
+        })
+
+        if(!conversation){
+            
+        }
+
+    } catch (error) {
+        console.log("Error in sendMessage controller: ", error.message)
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.js"
+import messageRoutes from "./routes/messageRoutes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -10,8 +11,10 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
 
 app.listen(PORT, () => {
     connectToMongoDB();
