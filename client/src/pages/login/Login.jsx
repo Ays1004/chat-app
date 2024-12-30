1dbcfa467565a3636145ade9b4ba7e9c1a0492cc
Ays1004 <yewaleayush@gmail.com>
2024-12-31 01:53:41 +0530

    .........

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -27,6 +27,13 @@ const Login = () => {
               className="input input-bordered w-full max-w-xs"
             />
           </div>
+          <a href="#" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
+            {"Don't"} have an account?
+          </a>
+
+          <div>
+            <button className="btn btn-block btn-sm mt-2">Login</button>
+          </div>
         </form>
       </div>
     </div>
