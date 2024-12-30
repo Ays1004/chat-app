12f960c3a722ce85cdafc94858d4368bf8d5b1ae
Ays1004 <yewaleayush@gmail.com>
2024-12-30 13:40:09 +0530

    added User fetch

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -50,8 +50,12 @@ export const getMessages = async (req, res) => {
     const conversation = await Conversation.findOne({
       participants: {$all: [senderId, userToChatId]},
     }).populate("messages");
+
+    if(!conversation) return res.status(200).json([]);
+
+    const messages = conversation.messages;
     
-    res.status(200).json(conversation.messages)
+    res.status(200).json(messages)
 
   } catch (error) {
     console.log("Error in getMessages controller: ", error.message);
