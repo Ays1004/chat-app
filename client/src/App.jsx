1af5f13dcdb172405b20e50c0d02be719e556a6b
Ays1004 <yewaleayush@gmail.com>
2024-12-31 14:12:33 +0530

    added stuff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 
 import "./App.css";
+import Home from "./pages/home/Home";
 
-import Signup from "./pages/signup/Signup";
+
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Signup />
+      <Home />
     </div>
   );
 }
