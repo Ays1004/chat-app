175f07d0ced459fee4e3cfb5bfb7f87b1b0d6589
Ays1004 <yewaleayush@gmail.com>
2025-01-04 02:09:03 +0530

    finished connecting backend to frontend

--- client/src/components/messages/Message.jsx
+++ client/src/components/messages/Message.jsx
@@ -1,17 +1,28 @@
-const Message = () => {
+import { useAuthContext } from "../../context/AuthContext";
+import useConversation from "../../store/useConversation";
+import { extractTime } from "../../utils/extractTime";
+
+const Message = ({message}) => {
+  const{authUser} = useAuthContext();
+  const {selectedConversation} = useConversation() 
+  const fromMe = message.senderId === authUser._id
+  const formattedTime = extractTime(message.createdAt)
+  const chatClassName = fromMe ? 'chat-end' : 'chat-start';
+  const profilePic = fromMe ? authUser.profilePic: selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? 'bg-blue-500' : "";
   return (
-    <div className="chat chat-end">
+    <div className={`chat ${chatClassName}`}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
           <img
             alt="Tailwind CSS chat bubble component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+            src={profilePic}
           />
         </div>
       </div>
-      <div className="chat-bubble">You were the Chosen One!</div>
+      <div className={`chat-bubble text-white ${bubbleBgColor}`}>{message.message}</div>
       <div className="chat-footer">
-        <time className="text-xs opacity-50">12:45</time>
+        <time className="text-xs opacity-50">{formattedTime}</time>
       </div>
     </div>
   );
